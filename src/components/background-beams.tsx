ab534352c8ad5e9c00671e1105aa96454d4959d1
prathmesh988 <prathmeshgawade41@gmail.com>
2024-08-08 15:45:24 +0530

    quick fix

--- src/components/background-beams.tsx
+++ src/components/background-beams.tsx
@@ -1,4 +1,4 @@
-"use client";
+
 import React from "react";
 import { motion } from "framer-motion";
 import { cn } from "../lib/util";
@@ -56,7 +56,7 @@ export const BackgroundBeams = React.memo(
             ></motion.path>
           ))}
           <defs>
-            {paths.map((path, index) => (
+            {paths.map((_path, index) => (
               <motion.linearGradient
                 id={`linearGradient-${index}`}
                 key={`gradient-${index}`}
